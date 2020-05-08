ca8ec50cdd0ab37678ad692d3f7a9b7fa76aaea7
cawauchi6204 <cawauchi6204@gmail.com>
2020-05-08 13:52:30 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores officia autem nisi placeat, voluptatibus dolores natus impedit harum, distinctio animi eaque enim ad facere magnam molestiae quod id at assumenda.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+}
+</script>
